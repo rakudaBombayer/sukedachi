5febe6a22bd00cee330a31a57b7e4905a14db830
rakudaBombayer <113893608+rakudaBombayer@users.noreply.github.com>
2025-01-25 21:37:57 +0900

    ChatMessage モデルファイルを書きました。

---
+++ oripro/app/Models/ChatMessage.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ChatMessage extends Model
+{
+    use HasFactory;
+    
+    protected $table = 'chat_messages';
+    protected $primaryKey = 'chat_message_ID';
+    protected $fillable = ['chat_room_ID', 'user_ID', 'text'];
+
+    public function chatRoom()
+    {
+        return $this->belongsTo(ChatRoom::class, 'chat_room_ID');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_ID');
+    }
+    
+}
