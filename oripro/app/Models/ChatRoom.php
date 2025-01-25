dddcb404fbf48ca041d637bfb5b6f2a7b5f8c652
rakudaBombayer <113893608+rakudaBombayer@users.noreply.github.com>
2025-01-25 20:07:52 +0900

    モデルのChatRoomファイルのリレーションを書きました。

---
+++ oripro/app/Models/ChatRoom.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+class ChatRoom extends Model
+{
+    protected $table = 'chat_rooms';
+    protected $primaryKey = 'chat_room_ID';
+    protected $fillable = ['chat_room_ID','user_ID'];
+
+
+    public function messages()
+    {
+        return $this->hasMany(ChatMessage::class, 'chat_room_ID');
+    }
+
+    public function request()
+    {
+        return $this->belongsTo(Request::class, 'request_ID');
+    }
+
+}
