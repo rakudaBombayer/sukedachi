49b40a1c4ec0247f700cff9ea1c7843f119dea5c
rakudaBombayer <113893608+rakudaBombayer@users.noreply.github.com>
2025-01-28 23:51:35 +0900

    先にコントローラーファイルをつくる

--- oripro/routes/web.php
+++ oripro/routes/web.php
@@ -4,4 +4,28 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+});// routes/web.php
+
+// Userルート
+Route::resource('users', UserController::class);
+
+// ChatRoomルート
+Route::resource('chat_rooms', ChatRoomController::class);
+
+// ChatMessageルート（ネストされたリソースルート）
+Route::resource('chat_rooms.messages', ChatMessageController::class);
+
+// Requestルート
+Route::resource('requests', RequestController::class);
+
+// Applicantルート
+Route::resource('applicants', ApplicantController::class);
+
+// HelpCategoryルート
+Route::resource('help_categories', HelpCategoryController::class);
+
+// Imageルート
+Route::resource('images', ImageController::class);
+
+// Paymentルート
+Route::resource('payments', PaymentController::class);
