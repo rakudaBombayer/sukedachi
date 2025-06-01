a1aa3ccd13c4906545c21171c4d9059b64d05246
rakudaBombayer <113893608+rakudaBombayer@users.noreply.github.com>
2025-06-01 09:51:00 +0900

    ログイン画面と登録画面と認証を作りました。

--- oripro/routes/web.php
+++ oripro/routes/web.php
@@ -1,47 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\ChatRoomController;
-use App\Http\Controllers\ChatMessageController;
-use App\Http\Controllers\RequestController;
-use App\Http\Controllers\ApplicantController;
-use App\Http\Controllers\HelpCategoryController;
-use App\Http\Controllers\ImageController;
-use App\Http\Controllers\PaymentController;
-use App\Http\Controllers\YourHomeController;
 
+Route::get('/', function () {
+    return view('welcome');
+});
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-// Userルート
-Route::resource('users', UserController::class);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-//ホーム画面へ
-Route::get('/', [YourHomeController::class, 'index'])->name('index');
-
-
-// ChatRoomルート
-Route::resource('chat_rooms', ChatRoomController::class);
-
-Route::post('/chat_rooms/goto/{request}', [ChatRoomController::class, 'gotoChat'])->name('chat_rooms.goto');
-
-// ChatMessageルート（ネストされたリソースルート）
-Route::resource('chat_rooms.messages', ChatMessageController::class);
-
-Route::post('/chat-messages', [ChatMessageController::class, 'store'])->name('chat_messages.store');
-
-// Requestルート
-Route::resource('requests', RequestController::class);
-
-Route::get('/complete/{request}', [RequestController::class, 'complete'])->name('requests.complete');
-
-Route::post('/requests', [RequestController::class, 'store'])->name('requests.store');
-
-// Applicantルート
-Route::resource('applicants', ApplicantController::class);
-
-// HelpCategoryルート
-Route::resource('help_categories', HelpCategoryController::class);
-
-// Imageルート
-Route::resource('images', ImageController::class);
+require __DIR__.'/auth.php';
