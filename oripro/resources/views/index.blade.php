5391c535f3ee8fd31cde9fb3f11ff8a1f2f408a1
rakudaBombayer <113893608+rakudaBombayer@users.noreply.github.com>
2025-06-28 20:51:32 +0900

    ホーム画面のコメントアウトを消しました。

--- oripro/resources/views/index.blade.php
+++ oripro/resources/views/index.blade.php
@@ -14,13 +14,7 @@
     <nav>
         <ul>
             <li><a href="{{ route('requests.create') }}" style="color: red;">手伝って</a></li>
-            {{-- <li><a href="{{ route('requests.index') }}">ニックネーム</a></li>
-            <li><a href="{{ route('requests.index') }}">リクエスト一覧</a></li>
-            <li><a href="{{ route('applicants.index') }}">応募者一覧</a></li>
-            <li><a href="{{ route('applicants.index') }}">応募者一覧</a></li>
-            <li><a href="{{ route('applicants.index') }}">応募者一覧</a></li>
-            <li><a href="{{ route('images.index') }}">画像一覧</a></li>
-            <li><a href="{{ route('chat_rooms.index') }}" style="color: red;">手伝う</a></li> --}}
+
 
             @auth
                 <p>ようこそ、{{ Auth::user()->nickname }} さん！</p>
