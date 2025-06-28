7165d52004ef76e1ea175f1b238e208e535c6cea
rakudaBombayer <113893608+rakudaBombayer@users.noreply.github.com>
2025-06-28 15:08:27 +0900

    アカウント登録画面への遷移ボタン完成

--- oripro/resources/views/auth/login.blade.php
+++ oripro/resources/views/auth/login.blade.php
@@ -33,6 +33,13 @@
             </label>
         </div>
 
+
+        <div class="flex items-center justify-end mt-4">
+            <a href="{{ route('register') }}" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            {{ __('新規登録はこちら') }}
+            </a>
+        </div>
+
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
