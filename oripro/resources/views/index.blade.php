f64be1ca72ffd4cdbf32a0b9d15eb9119b837ab8
rakudaBombayer <113893608+rakudaBombayer@users.noreply.github.com>
2025-06-29 13:29:19 +0900

    ホーム画面にいったんcssを入れました。

--- oripro/resources/views/index.blade.php
+++ oripro/resources/views/index.blade.php
@@ -1,6 +1,6 @@
-{{-- @extends('layouts.app')
+@extends('layouts.app')
 
-@section('content') --}}
+@section('content')
 
 <!DOCTYPE html>
 <html>
@@ -41,14 +41,45 @@
 
     <div>
         <h2>投稿された依頼</h2>
-        @if (isset($allRequests) && count($allRequests) > 0)
-            <ul>
-                @foreach ($allRequests as $request)
-                    <li><a href="{{ route('requests.show', $request->request_ID) }}">{{ $request->title }}</a> - {{ $request->general_area }}</li>
-                @endforeach
-            </ul>
+        <div class="max-w-5xl mx-auto px-4">
+            @if (isset($allRequests) && count($allRequests) > 0)
+            {{-- <div class="max-w-5xl mx-auto px-4"> --}}
+            {{-- < class="grid grid-cols-1 md:grid-cols-2 gap-4"> --}}
+
+            {{-- ↓仮 --}}
+            <div class="flex flex-wrap gap-4">
+            @foreach ($allRequests as $request)
+                
+                    <div class="w-[30%] min-w-[200px] bg-white shadow rounded-md p-4 hover:bg-gray-50 transition">
+                        <a href="{{ route('requests.show', $request->request_ID) }}" class="font-semibold text-blue-600 hover:underline block">
+                            {{ $request->title }}
+                        </a>
+                        <p class="text-sm text-gray-600 mt-1">場所 {{ $request->general_area }}</p>
+                    </div>
+                            
+            @endforeach
+            </div>
+
+
+                <p>{{ count($allRequests) }} 件の投稿</p>
+                <div class="grid grid-cols-2 gap-4">
+                <div class="bg-yellow-200 p-4">カード1</div>
+                <div class="bg-yellow-200 p-4">カード2</div>
+                <div class="bg-yellow-200 p-4">カード3</div>
+                <div class="bg-red-300 p-4">A</div>
+                </div>
+
+
+            
+            <div class="grid grid-cols-2 gap-4 bg-yellow-200 p-4">
+                <div class="bg-red-500 p-4">A</div>
+                <div class="bg-blue-500 p-4">B</div>
+            </div>
         @else
             <p>まだ投稿された依頼はありません。</p>
         @endif
+        </div>
+
+
     </div>
-{{-- @endsection --}}
+@endsection
