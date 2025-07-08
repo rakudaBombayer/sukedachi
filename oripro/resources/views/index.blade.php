d0da17e8530dc73614ed91496d8bf13dbd16d321
rakudaBombayer <113893608+rakudaBombayer@users.noreply.github.com>
2025-07-08 22:53:29 +0900

    ボタンの大きさのレイアウトやレスポンシブを行った。

--- oripro/resources/views/index.blade.php
+++ oripro/resources/views/index.blade.php
@@ -2,12 +2,9 @@
 
 @section('content')
 
-
-
     <nav>
         <ul>
-            <li><a href="{{ route('requests.create') }}" style="color: red;">手伝って</a></li>
-
+            {{-- <li><a href="{{ route('requests.create') }}" style="color: red;">手伝って</a></li> --}}
 
             @auth
                 <p>ようこそ、{{ Auth::user()->nickname }} さん！</p>
@@ -16,9 +13,9 @@
             @csrf
                 </form>
 
-            <a href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+            {{-- <a href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                 ログアウト
-            </a>
+            </a> --}}
 
             @else
                 <p>ログインしていません。</p>
@@ -28,14 +25,19 @@
     </nav>
 
     <div>
-        <h2>投稿された依頼</h2>
+
         <div class="max-w-5xl mx-auto px-4">
+            {{-- ↓ボタンのレスポンシブはあとでカスタマイズする --}}
+            <button onclick="location.href='{{ route('requests.create') }}'" class="bg-[#FF9D9D] text-2xl text-white font-black px-6 py-4 my-6 sm:my-6 md:my-8 lg:my-12 shadow rounded-full border  hover:bg-[#fb9292]  border border-[#bb6d6d] transition w-full max-w-[400px] mx-auto">手伝って</button>
+
+
             @if (isset($allRequests) && count($allRequests) > 0)
             {{-- <div class="max-w-5xl mx-auto px-4"> --}}
             {{-- < class="grid grid-cols-1 md:grid-cols-2 gap-4"> --}}
 
             {{-- ↓仮 --}}
             <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mb-8">
+                
                 @foreach ($allRequests as $request)
                     <div class="bg-[#FEE1E1] shadow rounded-md p-4 hover:bg-[#FF9D9D] transition aspect-square min-h-[100px]">
                         <a href="{{ route('requests.show', $request->request_ID) }}" class="font-semibold text-black hover:underline block">{{ $request->title }}</a>
