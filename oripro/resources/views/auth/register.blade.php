3188359bcc8a82683bbf976acb7366b464e26e30
rakudaBombayer <113893608+rakudaBombayer@users.noreply.github.com>
2025-07-09 00:21:32 +0900

    新規登録画面のUI完成

--- oripro/resources/views/auth/register.blade.php
+++ oripro/resources/views/auth/register.blade.php
@@ -1,66 +1,92 @@
-<x-guest-layout>
+@extends('layouts.app')
+
+@section('content')
+
+
+<div class="min-h-screen flex justify-center pt-12 bg-white">
+    <div class="w-full max-w-md mx-auto p-4">
+        <div class="flex flex-col gap-2 items-center">
+            <div class="font-bold text-2xl mb-2">
+                新規登録画面
+            </div>
+        </div>
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
         <!-- Name -->
-        <div>
-            <x-input-label for="nickname" :value="__('Nickname')" />
-            <x-text-input id="nickname" class="block mt-1 w-full" type="text" name="nickname" :value="old('nickname')" required autofocus autocomplete="nickname" />
+        <div class="flex flex-col gap-2 items-center">
+            <x-input-label for="nickname" :value="__('ニックネーム')" />
+            <x-text-input id="nickname" class="block mt-1 w-full" type="text" name="nickname" :value="old('nickname')" required autofocus autocomplete="nickname" class="w-3/4 bg-[#F2EEEE] border border-[#F2EEEE] rounded-full px-4 py-2"/>
             <x-input-error :messages="$errors->get('nickname')" class="mt-2" />
         </div>
 
-        <div>
-            <x-input-label for="address" :value="__('Address')" />
-            <x-text-input id="address" class="block mt-1 w-full" type="text" name="address" :value="old('address')" required autofocus autocomplete="address" />
+        <div class="mt-4 flex flex-col gap-2 items-center">
+            <x-input-label for="address" :value="__('だいたい住所')" />
+            <x-text-input id="address" class="block mt-1 w-full" type="text" name="address" :value="old('address')" 
+            placeholder="市区町村まで"
+            required autofocus autocomplete="address" class="w-3/4 bg-[#F2EEEE] border border-[#F2EEEE] rounded-full px-4 py-2"/>
             <x-input-error :messages="$errors->get('address')" class="mt-2" />
         </div>
 
 
         <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+        <div class="mt-4 flex flex-col gap-2 items-center">
+            <x-input-label for="email" :value="__('メールアドレス')" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" class="w-3/4 bg-[#F2EEEE] border border-[#F2EEEE] rounded-full px-4 py-2"/>
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
-        <div>
-            <x-input-label for="self_introduction" :value="__('Self_introduction')" />
-            <textarea id="self_introduction" class="block mt-1 w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-500 focus:ring-indigo-500" name="self_introduction" rows="5" required autofocus autocomplete="self_introduction">{{ old('self_introduction') }}</textarea>
+        <div class="mt-4 flex flex-col gap-2 items-center">
+            <x-input-label for="self_introduction" :value="__('自己紹介')" />
+            <textarea id="self_introduction" class="block mt-1 w-3/4 rounded-md shadow-sm bg-[#F2EEEE] border-[#F2EEEE] focus:border-indigo-500 focus:ring-indigo-500" name="self_introduction" rows="5" required  autofocus autocomplete="self_introduction">{{ old('self_introduction') }}</textarea>
             <x-input-error :messages="$errors->get('self_introduction')" class="mt-2" />
         </div>
 
 
         <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+        <div class="mt-4 flex flex-col gap-2 items-center">
+            <x-input-label for="password" :value="__('パスワード')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
                             name="password"
-                            required autocomplete="new-password" />
+                            placeholder="8桁以上の英数字でお願いします。"
+                            required autocomplete="new-password" 
+                            class="w-3/4 bg-[#F2EEEE] border border-[#F2EEEE] rounded-full px-4 py-2"
+                            />
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
         <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+        <div class="mt-4 flex flex-col gap-2 items-center">
+            <x-input-label for="password_confirmation" :value="__('確認用パスワード')" />
 
             <x-text-input id="password_confirmation" class="block mt-1 w-full"
                             type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
+                            name="password_confirmation" required autocomplete="new-password" 
+                            placeholder="上と同じものを入力してください。"
+                            class="w-3/4 bg-[#F2EEEE] border border-[#F2EEEE] rounded-full px-4 py-2"
+                            />
 
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+        </div>
+        <div class="mt-4 flex flex-col gap-2 items-center">
+            <button class="w-3/4 bg-[#FF9D9D] text-white font-bold mb-2 px-4 py-2 rounded-[30px] hover:bg-[#fd8f8f] transition">
+                {{ __('登録') }}
+            </button>
         </div>
 
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
+                {{ __('すでに登録済みの方はこちら') }}
             </a>
 
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
+            
         </div>
     </form>
-</x-guest-layout>
+  </div>
+</div>
+
+@endsection
+
